4231750b39bfabc007a32de6b3628de179f7515a
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-04-05 14:46:05 +0530

    dashbaord

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ require('./jobs/autoCancelJob');
 
 // Middleware
 app.use(cors({ origin: '*' }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
